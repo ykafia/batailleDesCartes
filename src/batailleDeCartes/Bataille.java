eb72dd67ac1f6b7f5d96dec7177f2aa12537469f
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2022-10-05 23:11:19 +0200

    Update Bataille.java

--- src/batailleDeCartes/Bataille.java
+++ src/batailleDeCartes/Bataille.java
@@ -2,24 +2,33 @@ package batailleDeCartes;
 
 public class Bataille {
 	public static void main(String[] args) {
+		Joueurs joueur1 = new Joueurs();
+		Joueurs joueur2 = new Joueurs();
 		
-		public void afficherCarte() {
-			System.out.printf("la %d de %s",t,couleur);
+		for(int i = 0; i < 10; i++){
+			joeurs1.ajouter(new Carte());
 		}
-		
-		//create g
-		Jouer p1= new Joueur();
+		// On utilise une autre boucle pour éviter d'avoir les mêmes valeurs si les randoms sont créés ~en même temps
+		for(int i = 0; i < 10; i++){
+			joeurs2.ajouter(new Carte());
+		}
+		while(joueur1.getNombreCarte() > 0 || joueur2.getNombreCarte() > 0){
+			var carte1 = joueur1.tirer();
+			var carte2 = joueur2.tirer();
+			System.out.println("Joueur1 joue : " + carte1.toString());
+			System.out.println("Joueur2 joue : " + carte2.toString());
+			var compare = carte1.compare(carte2);
+			joueur1.score(compare);
+			joueur2.score(-compare);
+			System.in.read()
 			
-			//Print the start of the game
-			//in a while loop, do the game with the score showing
-			
-			//show the winner
-			public void vainqueur() {};
-			
-			
-			
-		
-		
+		}
+		if(joueur1.getScore() > joueur2.getScore())
+			System.out.println("Joueur1 gagne");
+		if(joueur1.getScore() < joueur2.getScore())
+			System.out.println("Joueur2 gagne");
+		else
+			System.out.println("Same same");
 		
 	
 	}
